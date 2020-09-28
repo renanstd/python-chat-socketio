112578993c92a15800c224ae68b72a2108f4190b
Renan Santana <renan_ota@hotmail.com>
2020-09-28 01:00:55 -0300

    Corrige string

--- src/static/session.js
+++ src/static/session.js
@@ -1,5 +1,4 @@
-var protocol = window.location.protocol;
-var socket = io.connect(protocol + '//' + document.domain); //  + ':' + location.port
+var socket = io.connect('https://' + document.domain); //  + ':' + location.port
 
 socket.on('connect', function() {
     socket.emit('my event', {
